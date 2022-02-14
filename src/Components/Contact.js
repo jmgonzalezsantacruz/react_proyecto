961630ab23eda098e27b4d9aec13a2ad28e93e6a
jmgonzalezsantacruz <jm.gonzalezsantacruz@gmail.com>
2022-02-14 12:20:02 +0100

    Subida día 14

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -87,7 +87,8 @@ class Contact extends Component {
 					   </p>
 				   </div>
 
-               <div className="widget widget_tweets">
+               {/* TWITTER
+               <div className="widget widget_tweets"> 
                   <h4 className="widget-title">Latest Tweets</h4>
                   <ul id="twitter">
                      <li>
@@ -107,7 +108,7 @@ class Contact extends Component {
                         <b><a href="#">3 Days Ago</a></b>
                      </li>
                   </ul>
-		         </div>
+		         </div>*/}
             </aside>
       </div>
    </section>
